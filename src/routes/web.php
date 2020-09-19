206b3b4b89af07cb27359c6f16ea4db90cabe6f7
shotasan <wondermoai@gmail.com>
2020-09-19 21:46:03 +0900

    12

--- src/routes/web.php
+++ src/routes/web.php
@@ -13,29 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
-// Route::get('/', function () {
-//   return view('home');
-// });
-
-Route::view('/', 'home')->name('home');
-
-// Route::get('/contact', function () {
-//   return view('contact');
-// });
-
-Route::view('/contact', 'contact')->name('contact');
-
-Route::get('/blog-post/{id}/{welcome?}', function ($id, $welcome = 1) {
-  $pages = [
-    1 => [
-      'title' => 'from page 1',
-    ],
-    2 => [
-      'title' => 'from page 2',
-    ],
-  ];
-  $welcomes = [
-    1 => 'Hello ', 2 => 'Welcome to ',
-  ];
-  return view('blog-post', ['data' => $pages[$id], 'welcome' => $welcomes[$welcome]]);
-})->name('blog-post');
+Route::get('/', 'HomeController@home')->name('home');
+Route::get('/contact', 'HomeController@contact')->name('contact');
+Route::get('/blog-post/{id}/{welcome?}', 'HomeController@blogPost')->name('blog-post');
